eff74c68377f17cd8aa6d8350b24f60a54679c14
baohmq <baohmq@xanhsm.com>
2025-12-05 22:26:24 +0700

    collage screen

--- app/src/main/java/com/avnsoft/photoeditor/photocollage/ui/activities/store/tab/template/detail/TemplateDetailActivity.kt
+++ app/src/main/java/com/avnsoft/photoeditor/photocollage/ui/activities/store/tab/template/detail/TemplateDetailActivity.kt
@@ -324,32 +324,7 @@ fun TemplateDetailContent(
 
             // Layers overlay - Layer 3
             template.layer?.forEachIndexed { index, layerItem ->
-                val x = layerItem.x ?: 0f
-                val y = layerItem.y ?: 0f
-                val width = layerItem.width ?: 0f
-                val height = layerItem.height ?: 0f
-                val rotate = layerItem.rotate?.toFloat() ?: 0f
-
-                val layerX = x * bannerWidth
-                val layerY = y * bannerHeight
-                val layerWidth = width * bannerWidth
-                val layerHeight = height * bannerHeight
-
-                Box(
-                    modifier = Modifier
-                        .offset(x = with(density) { layerX.toDp() }, y = with(density) { layerY.toDp() })
-                        .width(with(density) { layerWidth.toDp() })
-                        .height(with(density) { layerHeight.toDp() })
-                        .rotate(rotate)
-                ) {
-                    layerItem.urlThumb?.let { urlThumb ->
-                        LoadImage(
-                            model = urlThumb,
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
-                }
+                //import layer
             }
         }
 
