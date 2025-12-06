eef8f6a9ec2df7728048fb57eaf47c375aa3ad0d
OFFICEVNPAY\thanhnc2 <thanhnc2@vnpay.vn>
2025-12-06 23:06:31 +0700

    ui

--- app/src/main/java/com/avnsoft/photoeditor/photocollage/data/model/pattern/PatternModel.kt
+++ app/src/main/java/com/avnsoft/photoeditor/photocollage/data/model/pattern/PatternModel.kt
@@ -1,26 +1,41 @@
 package com.avnsoft.photoeditor.photocollage.data.model.pattern
 
 import com.basesource.base.ui.base.IScreenData
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PatternModel(
+    @SerializedName("eventId")
     val eventId: Long,
+    @SerializedName("urlThumb")
     val urlThumb: String?,
+    @SerializedName("tabName")
     val tabName: String,
+    @SerializedName("content")
     val content: List<PatternContentModel>,
+    @SerializedName("isPro")
     val isPro: Boolean,
+    @SerializedName("isFree")
     val isFree: Boolean,
+    @SerializedName("isReward")
     val isReward: Boolean,
+    @SerializedName("timeCreate")
     val timeCreate: String = System.currentTimeMillis().toString(),
+    @SerializedName("isUsed")
     val isUsed: Boolean,
+    @SerializedName("total")
     val total: String,
+    @SerializedName("bannerUrl")
     val bannerUrl: String?
 ) : IScreenData
 
 @Serializable
 data class PatternContentModel(
+    @SerializedName("title")
     val title: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("urlThumb")
     val urlThumb: String,
 )
--- app/src/main/java/com/avnsoft/photoeditor/photocollage/data/model/sticker/StickerModel.kt
+++ app/src/main/java/com/avnsoft/photoeditor/photocollage/data/model/sticker/StickerModel.kt
@@ -1,23 +1,37 @@
 package com.avnsoft.photoeditor.photocollage.data.model.sticker
 
 import com.basesource.base.ui.base.IScreenData
-
+import com.google.gson.annotations.SerializedName
 data class StickerModel(
+    @SerializedName("eventId")
     val eventId: Long,
+    @SerializedName("iconTabUrl")
     val iconTabUrl: String?,
+    @SerializedName("tabName")
     val tabName: String,
+    @SerializedName("content")
     val content: List<StickerContentModel>,
+    @SerializedName("isPro")
     val isPro: Boolean,
+    @SerializedName("isFree")
     val isFree: Boolean,
+    @SerializedName("isReward")
     val isReward: Boolean,
+    @SerializedName("timeCreate")
     val timeCreate: String = System.currentTimeMillis().toString(),
+    @SerializedName("isUsed")
     val isUsed: Boolean,
+    @SerializedName("total")
     val total: String,
+    @SerializedName("bannerUrl")
     val bannerUrl: String?
 ) : IScreenData
 
 data class StickerContentModel(
+    @SerializedName("title")
     val title: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("urlThumb")
     val urlThumb: String,
 )
