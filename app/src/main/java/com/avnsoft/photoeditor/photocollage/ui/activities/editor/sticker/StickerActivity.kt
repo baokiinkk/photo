5adefd5b9910198b1c4e16fd2f681471b735e4b2
OFFICEVNPAY\thanhnc2 <thanhnc2@vnpay.vn>
2025-12-07 22:18:25 +0700

    ui

--- app/src/main/java/com/avnsoft/photoeditor/photocollage/ui/activities/editor/sticker/StickerActivity.kt
+++ app/src/main/java/com/avnsoft/photoeditor/photocollage/ui/activities/editor/sticker/StickerActivity.kt
@@ -173,6 +173,7 @@ class StickerActivity : BaseActivity() {
                             finish()
                         },
                         onApply = {
+                            viewmodel.showLoading()
                             stickerView?.setShowFocus(false){
                                 stickerView?.post {
                                     captureView(
@@ -199,6 +200,7 @@ class StickerActivity : BaseActivity() {
                                             )
                                         }
                                     )
+                                    viewmodel.hideLoading()
                                 }
                             }
                         },
